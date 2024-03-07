e7d47d2c0aaff7ef8eebbf01d511a4c3941ae749
Milena Rickli Silvério <141282223+MilenaRickliS@users.noreply.github.com>
2024-03-07 20:46:48 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,176 +1,315 @@
-import { useState, useEffect } from "react";
-import { db, auth} from './firebaseConnection';
-import './App.css'
+import { useState, useEffect } from 'react'
+import { db, auth } from './firebaseConnection';
 
-import {
+import { 
   doc, 
-  setDoc,
-  collection,
-  addDoc,
-  getDoc,
-  getDocs,
-  updateDoc,
+  setDoc, 
+  collection, 
+  addDoc, 
+  getDoc, 
+  getDocs, 
+  updateDoc, 
   deleteDoc,
-  onSnapshot,
-  snapshotEqual
-} from 'firebase/firestore';
+  onSnapshot
+} from 'firebase/firestore'
 
-import{
+import { 
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   signOut,
   onAuthStateChanged
-} from 'firebase/auth';
+} from 'firebase/auth'
 
-function App(){
-  const [titulo, setTitulo] = useState("")
-  const [autor, setAutor] = useState('')
+import './App.css';
+
+function App() {
+  const [titulo, setTitulo] = useState('');
+  const [autor, setAutor] = useState('');
   const [idPost, setIdPost] = useState('')
-  const [email, setEmail] = useState('')
-  const [senha, setSenha] = useState('')
-  
-  const [posts, setPosts] = useState([])
-  
-  const [user, setUser] = useState(false)
-  const [userDetail, setUserDetail] = useState([])
-  
-  useEffect(() =>{
-    async function carregarPosts(){
-      const dados = onSnapshot(collection(db, 'posts'), (snapshot) => { //snapshot percorre os dados no banco
-        let listaPost = []
+
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+
+  const [user, setUser] = useState(false);
+  const [userDetail, setUserDetail] = useState({})
+
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    async function loadPosts(){
+      const unsub = onSnapshot(collection(db, "posts"), (snapshot) => {
+        let listaPost = [];
+
         snapshot.forEach((doc) => {
           listaPost.push({
-            idPost: doc.id,
+            id: doc.id,
             titulo: doc.data().titulo,
-            autor: doc.data().autor
-          });
-        });
+            autor: doc.data().autor,
+          })
+        })
+  
         setPosts(listaPost);
       })
     }
-    carregarPosts();
-  },[])
 
-  useEffect(() =>{
-    async function verificarLogin(){
+    loadPosts();
+
+  }, [])
+
+  useEffect(() => {
+    async function checkLogin(){
       onAuthStateChanged(auth, (user) => {
         if(user){
-          //usuário logado
+          // se tem usuario logado ele entra aqui...
+          console.log(user);
           setUser(true);
           setUserDetail({
-            id: user.id,
+            uid: user.uid,
             email: user.email
-          });
-        } else{
+          })
+
+        }else{
+          // nao possui nenhum user logado.
           setUser(false);
-          setUserDetail({});
+          setUserDetail({})
         }
       })
     }
-    verificarLogin();
-  },[]);
 
-  //adicionar o post (create)
-  async function criarPost(){
-    await addDoc(collection(db, "posts"),{
+    checkLogin();
+
+  }, [])
+
+
+  async function handleAdd(){
+    // await setDoc(doc(db, "posts", "12345"), {
+    //   titulo: titulo,
+    //   autor: autor,
+    // })
+    // .then(() => {
+    //   console.log("DADOS REGISTRADO NO BANCO!")
+    // })
+    // .catch((error) => {
+    //   console.log("GEROU ERRO" + error)
+    // }) 
+
+
+    await addDoc(collection(db, "posts"), {
       titulo: titulo,
-      autor: autor
-    }).then(() =>{
-      setAutor('')
-      setTitulo('')
-    }).catch((error) =>{
-      console.log("ERRO" + error)
+      autor: autor,
     })
+    .then(() => {
+      console.log("CADASTRADO COM SUCESSO")
+      setAutor('');
+      setTitulo('')
+    })
+    .catch((error) => {
+      console.log("ERRO " + error)
+    })
+
+
   }
 
-  //buscar os posts(read)
-  async function buscarPosts(){
-    const ListasPosts = collection(db, 'posts')
-    await getDocs(ListasPosts).then((snapshot) => {
+
+  async function buscarPost(){
+    // const postRef = doc(db, "posts", "vFvZAyFqebXFsFv0X89l")
+    // await getDoc(postRef)
+    // .then((snapshot) => {
+    //   setAutor(snapshot.data().autor)
+    //   setTitulo(snapshot.data().titulo)
+
+    // })
+    // .catch(()=>{
+    //   console.log("ERRO AO BUSCAR")
+    // })
+
+    const postsRef = collection(db, "posts")
+    await getDocs(postsRef)
+    .then((snapshot) => {
       let lista = [];
-      snapshot.forEach((doc)=>{
+
+      snapshot.forEach((doc) => {
         lista.push({
           id: doc.id,
           titulo: doc.data().titulo,
-          autor: doc.data().autor
-        });
-      });
+          autor: doc.data().autor,
+        })
+      })
+
       setPosts(lista);
-    }).catch((error) =>{
-      console.log('DEU RUIM' + error)
-    });
+
+    })
+    .catch((error) => {
+      console.log("DEU ALGUM ERRO AO BUSCAR")
+    })
+
+
   }
 
-  //editar post (update)
-  async function editarPost(id){
-    const docRef = doc(db, 'posts', idPost)
+
+  async function editarPost(){
+    const docRef = doc(db, "posts", idPost)
+    
     await updateDoc(docRef, {
       titulo: titulo,
       autor: autor
-    }).then(() => {
-      console.log("post atualizado")
+    })
+    .then(() => {
+      console.log("POST ATUALIZADO!")
       setIdPost('')
       setTitulo('')
       setAutor('')
-    }).catch((error) => {
+    })
+    .catch((error) => {
       console.log(error)
     })
+
+
   }
 
-  //excluir post (delete)
+
   async function excluirPost(id){
-    const docRef = doc(db, 'posts', idPost)
+    const docRef = doc(db, "posts", id)
     await deleteDoc(docRef)
-    .then(() => {
-      alert('post deletado')
+    .then(() =>{
+      alert("POST DELETADO COM SUCESSO!")
     })
-    .catch((error) =>{
-      console.log(error)
+
+  }
+
+  async function novoUsuario(){
+    await createUserWithEmailAndPassword(auth, email, senha)
+    .then(() => {
+      console.log("CADASTRADO COM SUCESSO!")
+    
+      setEmail('')
+      setSenha('')
+    })
+    .catch((error) => {
+      
+      if(error.code === 'auth/weak-password'){
+        alert("Senha muito fraca.")
+      }else if(error.code === 'auth/email-already-in-use'){
+        alert("Email já existe!")
+      }
+
     })
   }
 
-  return(
+  async function logarUsuario(){
+    await signInWithEmailAndPassword(auth, email, senha)
+    .then((value) => {
+      console.log("USER LOGADO COM SUCESSO")
+      console.log(value.user);
+
+      setUserDetail({
+        uid: value.user.uid,
+        email: value.user.email,
+      })
+      setUser(true);
+
+      setEmail('')
+      setSenha('')
+    })
+    .catch(() => {
+      console.log("ERRO AO FAZER O LOGIN")
+    })
+  }
+
+
+  async function fazerLogout(){
+    await signOut(auth)
+    setUser(false);
+    setUserDetail({})
+  }
+
+  return (
     <div>
-      <h1>React JS + Firebase</h1>
-      <div className="container">
-        <h2>POSTS</h2>
-        <label>ID do Post:</label>
-        <input placeholder="Digite o ID"
+      <h1>ReactJS + Firebase :)</h1>
+
+      { user && (
+        <div>
+          <strong>Seja bem-vindo(a) (Você está logado!)</strong> <br/>
+          <span>ID: {userDetail.uid} - Email: {userDetail.email}</span> <br/>
+          <button onClick={fazerLogout}>Sair da conta</button>
+          <br/> <br/>
+        </div>
+      )}
+
+    <div className="container">
+      <h2>Usuarios</h2>
+
+      <label>Email</label>
+      <input 
+        value={email}
+        onChange={(e) => setEmail(e.target.value)} 
+        placeholder="Digite um email"
+      /> <br/>
+
+      <label>Senha</label>
+      <input 
+        value={senha}
+        onChange={(e) => setSenha(e.target.value)} 
+        placeholder="Informe sua senha"
+      /> <br/> 
+
+      <button onClick={novoUsuario}>Cadastrar</button>     
+      <button onClick={logarUsuario}>Fazer login</button>     
+    </div>
+
+    <br/><br/>
+    <hr/>
+
+
+    <div className="container">
+      <h2>POSTS</h2>
+
+      <label>ID do Post:</label>
+      <input
+        placeholder='Digite o ID do post'
         value={idPost}
-        onChange={(e) => setIdPost(e.target.value)}/>
+        onChange={ (e) => setIdPost(e.target.value) }
+      /> <br/>
 
-        <label>Titulo</label>
-        <textarea type='text' placeholder="Digite o Titulo"
+      <label>Titulo:</label>
+      <textarea 
+        type="text"
+        placeholder='Digite o titulo'
         value={titulo}
-        onChange={(e) => setTitulo(e.target.value)}/>
+        onChange={ (e) => setTitulo(e.target.value) }
+      />
 
-        <label>Autor</label>
-        <input type="text" placeholder="Digite o Autor do Post"
+      <label>Autor:</label>
+      <input 
+        type="text" 
+        placeholder="Autor do post"
         value={autor}
-        onChange={(e) => setAutor(e.target.value)}/>
+        onChange={(e) => setAutor(e.target.value) }
+      />
 
-        <button onClick={criarPost}>Cadastrar</button>
-        <button onClick={buscarPosts}>Buscar</button>
-        <button onClick={editarPost(idPost)}>Editar</button>
-        
-        <ul>
-          {posts.map((post) => {
-            return(
-              <li key={post.id}>
-                <strong>ID: {post.id}</strong>
-                <strong>Titulo: {post.titulo}</strong>
-                <span>Autor: {post.autor}</span>
-                <button onClick={excluirPost(post.id)}>Excluir</button>
-              </li>
-            )
-          })}
-        </ul>
+      <button onClick={handleAdd}>Cadastrar</button>
+      <button onClick={buscarPost}>Buscar post</button> <br/>
 
-      </div>
+      <button onClick={editarPost}>Atualizar post</button>
+
+
+      <ul>
+        {posts.map( (post) => {
+          return(
+            <li key={post.id}>
+              <strong>ID: {post.id}</strong> <br/>
+              <span>Titulo: {post.titulo} </span> <br/>
+              <span>Autor: {post.autor}</span> <br/> 
+              <button onClick={ () => excluirPost(post.id) }>Excluir</button> <br/> <br/>
+            </li>
+          )
+        })}
+      </ul>
+
+    </div>
+
     </div>
   );
-  
-
 }
 
 export default App;
